4abda7e16cfd736829978627647a096c84ff6c05
yeeeeerim <gyr3904@gmail.com>
2023-05-22 21:14:48 +0900

    setting: Project Setting

---
+++ src/main/java/com/example/studycafeproject/StudycafeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.studycafeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycafeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycafeProjectApplication.class, args);
+	}
+
+}
